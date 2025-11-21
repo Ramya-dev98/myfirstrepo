c8267e61fd573c1dc47aba3ef5c57838cce21fba
Ramya-dev98 <ramyatejaswi1819@gmail.com>
2025-11-21 15:39:15 +0530

    dev 2 pushed the code

--- app.java
+++ app.java
@@ -41,3 +41,14 @@ public class SampleProgram {
     }
 }
 
+// dev-2 pushed the code
+
+public class AddTwoIntegers {
+   public static void main(String[] args) {
+       int first = 10;
+       int second = 20;
+       int sum = first + second;
+       System.out.println("The sum is: " + sum);
+   }
+}
+
